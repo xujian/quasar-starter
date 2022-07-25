a60a4f235443fcfc0a7ec4f33f1f875505c99433
Mike Jian Xu <782135@qq.com>
2022-07-25 22:51:57 +0800

    rename MasterLayout

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,8 +3,13 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('layouts/MasterLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/IndexPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
